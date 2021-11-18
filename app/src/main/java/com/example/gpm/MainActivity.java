9b802072565b8e75ba6adfff14c57853294dcfb7
sarveshsolanke <sarveshsolanke@github.com>
2021-11-18 23:30:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gpm/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.gpm;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+
+class SensorActivity extends Activity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private final float[] accelerometerReading = new float[3];
+    private final float[] magnetometerReading = new float[3];
+
+    private final float[] rotationMatrix = new float[9];
+    private final float[] orientationAngles = new float[3];
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
